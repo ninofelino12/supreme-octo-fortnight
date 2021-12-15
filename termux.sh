67f60aa51e19e85fe5f8ec174a283d9901ec883a
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2021-12-15 21:22:58 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -2,4 +2,6 @@ clear
 echo 'curl -s https://github.com/ninofelino12/supreme-octo-fortnight/blob/main/termux.sh | sh'
 echo 'Odoo in termux installer'
 echo '*********************************************'
-
+pkg update -y
+pkg install -y python nodejs yarn git
+yarn global add code-server
