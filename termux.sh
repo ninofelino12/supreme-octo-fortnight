68dad5746684a89b36f6931264fef2e29f5f935e
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2021-12-15 21:15:01 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,2 +1,5 @@
 clear
+echo 'curl -s https://github.com/ninofelino12/supreme-octo-fortnight/blob/main/termux.sh | sh'
 echo 'Odoo in termux installer'
+echo '*********************************************'
+
